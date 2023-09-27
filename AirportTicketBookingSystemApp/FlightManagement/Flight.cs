faf41af3fdf1c2de82a345c1c97c6e60c8480410
Ayah Shraim <ayahshraim558@gmail.com>
2023-09-27 11:57:30 +0300

    Add flight model, Implement initial fligt repository to read flights from file

---
+++ AirportTicketBookingSystemApp/FlightManagement/Flight.cs
@@ -0,0 +1,82 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystemApp.FlightManagement
+{
+    public class Flight
+    {
+        [Name("Flight Number")]
+        public int Number { get; set; }
+
+        [Name("Airline")]
+        public string Airline { get; set; } = String.Empty;
+
+        [Name("Dep Country")]
+        public string DepartureCountry { get; set; } = String.Empty;
+
+        [Name("Dep Airport")]
+        public string DepartureAirport { get; set; } = String.Empty;
+
+        [Name("Dep Date")]
+        public DateTime DepartureDate { get; set; }
+
+        [Name("Dep Time")]
+        public DateTime DepartureTime { get; set; }
+
+        [Name("Dep Day")]
+        public string DepartureDay { get; set; } = String.Empty;
+
+        [Name("Arrival Country")]
+        public string ArrivalCountry { get; set; } = String.Empty;
+
+        [Name("Arrival Airport")]
+        public string ArrivalAirport { get; set; } = String.Empty;
+
+        [Name("Arrival Date")]
+        public DateTime ArrivalDate { get; set; }
+
+        [Name("Arrival Time")]
+        public DateTime ArrivalTime { get; set; }
+
+        [Name("Arrival Day")]
+        public string ArrivalDay { get; set; } = String.Empty;
+
+        [Name("Duration")]
+        public TimeSpan Duration { get; set; }
+
+        [Name("Capacity")]
+        public int TotalCapacity { get; set; }
+
+        [Name("Economy Price")]
+        public int EconomiyPrice { get; set; }
+
+        [Name("Economy Capacity")]
+        public int EconomiyCapacity { get; set; }
+
+        [Name("Economy Available")]
+        public int EconomiyAvailable { get; set; }
+
+        [Name("Business Price")]
+        public int BusinessPrice { get; set; }
+
+        [Name("Business Capacity")]
+        public int BusinessCapacity { get; set; }
+
+        [Name("Business Available")]
+        public int BusinessAvailable { get; set; }
+
+        [Name("First Class Price")]
+        public int FirstClassPrice { get; set; }
+
+        [Name("First Class Capacity")]
+        public int FirstClassCapacity { get; set; }
+
+        [Name("First Class Available")]
+        public int FirstClassAvailable { get; set; }
+
+    }
+}
---
+++ AirportTicketBookingSystemApp/FlightManagement/FlightRepository.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystemApp.FlightManagement
+{
+    public class FlightRepository
+    {
+        public List<Flight> UploadFlights(string path)
+        {
+            List<Flight> flights;
+            try
+            {
+                using (var reader = new StreamReader(path))
+                {
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        flights = csv.GetRecords<Flight>().ToList();
+                    }
+
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("no such file");
+                flights = new List<Flight>();
+            }
+            return flights;
+        }
+    }
+}
