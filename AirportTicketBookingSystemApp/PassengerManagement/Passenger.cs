bc2feb277f9eecc0e142d7e1177490442a32ad23
Ayah Shraim <ayahshraim558@gmail.com>
2023-09-28 22:19:12 +0300

    Update validation of passenger model to use built in attributes

--- AirportTicketBookingSystemApp/PassengerManagement/Passenger.cs
+++ AirportTicketBookingSystemApp/PassengerManagement/Passenger.cs
@@ -1,19 +1,27 @@
 ﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
-namespace AirportTicketBookingTry.PassengerManagement
+namespace AirportTicketBookingSystemApp.PassengerManagement
 {
     public class Passenger
     {
         [Name("First Name")]
+        [Required(ErrorMessage ="First Name is required")]
         public string FirstName { get; set; } = string.Empty;
 
         [Name("Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; } = string.Empty;
 
         [Name("Email")]
+        [EmailAddress(ErrorMessage ="Not valid email address")]
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; } = string.Empty;
 
         [Name("Password")]
+        [Required(ErrorMessage = "Password is required")]
+        [PasswordPropertyText(true)]
         public string Password { get; set; } = string.Empty;
 
         public Passenger(string firstName, string lastName, string email, string password)
