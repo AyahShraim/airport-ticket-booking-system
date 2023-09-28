ec5f7d8be3598929c8ba89089adfbef4fb40cbde
Ayah Shraim <ayahshraim558@gmail.com>
2023-09-28 22:22:34 +0300

    Update add passenger function

--- AirportTicketBookingSystemApp/PassengerManagement/PassengerRepository.cs
+++ AirportTicketBookingSystemApp/PassengerManagement/PassengerRepository.cs
@@ -1,26 +1,25 @@
 ﻿using System.Globalization;
-using AirportTicketBookingTry.ResultHandler;
+using AirportTicketBookingSystemApp.ResultHandler;
 using CsvHelper;
 
 
-namespace AirportTicketBookingTry.PassengerManagement
+namespace AirportTicketBookingSystemApp.PassengerManagement
 {
     internal class PassengerRepository
     {
         private string _directory = @"C:\Users\DELL\source\repos\AirportTicketBookingSystem\AirportTicketBookingSystemApp\Data\";
         private string _usersFileName = "users.csv";
 
-        public OperationResult AddNewPassenger(string firstName, string lastName, string email, string password)
+        public OperationResult AddNewPassenger(Passenger passenger)
         {
-            if (IsExistPassenger(email))
+            if (IsExistPassenger(passenger.Email))
             {
                 return OperationResult.FailureResult("email already exist, try different email or login!");
             }
             using var writer = new StreamWriter($"{_directory}{_usersFileName}", append: true);
             using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
 
-            Passenger _passenger = new Passenger(firstName, lastName, email, password);
-            csvWriter.WriteRecord(_passenger);
+            csvWriter.WriteRecord(passenger);
             csvWriter.NextRecord();
 
             return OperationResult.SuccessResult("Account created successfully!");
