1b9f1239e7634674a5c7e4d57968db20cef2f3d1
Ayah Shraim <ayahshraim558@gmail.com>
2023-09-29 15:14:23 +0300

    add result handler new method to return result data

--- AirportTicketBookingSystemApp/ResultHandler/OperationResult.cs
+++ AirportTicketBookingSystemApp/ResultHandler/OperationResult.cs
@@ -1,14 +1,18 @@
-﻿namespace AirportTicketBookingTry.ResultHandler
+﻿namespace AirportTicketBookingSystemApp.ResultHandler
 {
     public class OperationResult
     {
-        public bool IsSuccess {  get; private set; }
-        public string Message  { get; private set; }
-
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+        public object Data { get; private set; } = new object();
         public OperationResult(bool isSuccess, string message)
         {
             IsSuccess = isSuccess;
             Message = message;
+        }
+        public OperationResult(bool isSuccess, string message, object data) : this(isSuccess, message)
+        {
+            Data = data;
         }
         public static OperationResult SuccessResult(string msg)
         {
@@ -18,6 +22,9 @@
         {
             return new OperationResult(false, msg);
         }
-
+        public static OperationResult SuccessDataMessage(string msg, object data)
+        {
+            return new OperationResult(true, msg, data);
+        }
     }
 }
