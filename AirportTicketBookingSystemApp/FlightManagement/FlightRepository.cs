6356aea78102b6a7102b75a92cda1d6a853a62b9
Ayah Shraim <ayahshraim558@gmail.com>
2023-10-04 11:26:58 +0300

    add flight validatior with invoking

--- AirportTicketBookingSystemApp/FlightManagement/FlightRepository.cs
+++ AirportTicketBookingSystemApp/FlightManagement/FlightRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AirportTicketBookingSystemApp.Enums;
 using CsvHelper;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
 namespace AirportTicketBookingSystemApp.FlightManagement
@@ -57,6 +58,61 @@ namespace AirportTicketBookingSystemApp.FlightManagement
 
             using var writer = new StreamWriter(Utilities.SystemFlightsPath);
             using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csvWriter.WriteRecords(_systemFlights);
+        }
+
+        public void BatchFlightUpload(string path)
+        {
+            try
+            {
+                LoadFlights(Utilities.SystemFlightsPath);
+                using (var reader = new StreamReader(path))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var newFlights = csv.GetRecords<Flight>().ToList();
+                    var isValid = true;
+
+                    foreach (var flight in newFlights)
+                    {
+                        Console.WriteLine($"Validating Flight {flight.Number}");
+                        var validationContext = new ValidationContext(flight, null, null);
+                        var validationResults = new List<ValidationResult>();
+
+                        if (!Validator.TryValidateObject(flight, validationContext, validationResults, true))
+                        {
+                            Console.WriteLine($"Validation Errors for Flight {flight.Number}:");
+                            foreach (var result in validationResults)
+                            {
+                                Console.WriteLine($" - {result.ErrorMessage}");
+                            }
+
+                            isValid = false;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        _systemFlights.AddRange(newFlights);
+                        AppendflightstoFile(Utilities.SystemFlightsPath);
+                        Console.WriteLine("Flights added Successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Check the erorr list.");
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Problem when trying to read the file.");
+            }
+        }
+
+        public void AppendflightstoFile(string path)
+        {
+            using var writer = new StreamWriter(path, append: false);
+            using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
             csvWriter.WriteRecords(_systemFlights);
         }
 
